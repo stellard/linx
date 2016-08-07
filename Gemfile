1dc8c7f9ed9965e452ea2e6ed53f7a132ac45042
stellard <scott.ellard@gmail.com>
2016-08-07 13:05:41 +0100

    rpsec fabrication added

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rspec-rails', '~> 3.5'
+  gem 'guard-rspec', require: false
+  gem 'fabrication'
 end
 
 group :development do
@@ -43,6 +46,3 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
-
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,21 @@ module Linx
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    #
+    #
+    #
+    #
+    #
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :rspec, :fixture => true, :view_specs => false, :controller_specs => false, :helper_specs => false, :routing_specs => false, :integration_tool => false
+      g.fixture_replacement :fabrication, :dir => "spec/fabricators"
+      g.helper false
+      g.stylesheets false
+      g.assets false
+      g.jbuilder false
+    end
   end
 end
